04746ee52729bb4e75fc41bcc764953fd8f2f007
John Goldsmith <goldsmith@uchicago.edu>
2017-04-04 22:12:20 +0200

    incomplete dynamics function

--- dynamics.py
+++ dynamics.py
@@ -1,6 +1,24 @@
 def dynamics( lex, corpus):
+    Signatures = dict()
+    StemToSig = dict()
     for word in corpus:
         siglist_for_word = lex.WordToSig[word]
         for (stem,sig) in siglist_for_word:
-            print stem, sig
+            affixlength = len(word) - len(stem)
+            affix = word[-1 * affixlength:]
+            print stem, affix,sig
+            if stem in StemToSig:
+                sig = StemToSig[stem]
+                if affix in sig:
+                    sig.StemToWordCount[sig][affix] += 1
+                    sig.Stem[stem] +=  1
+                else:
+                    #move stem to a bigger sig
+                    pass
+
+            else:
+                newsig = CSignature()
+
+
+
 
